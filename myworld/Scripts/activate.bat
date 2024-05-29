f07315eeddbf17d064f4096b42bc2813ee6bef40
Aseel-Attili <aseelattili2@gmail.com>
2024-05-29 21:36:12 +0300

    aseel

--- myworld/Scripts/activate.bat
+++ myworld/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lenovo\OneDrive\Desktop\New\Graduation-project\myworld
+set VIRTUAL_ENV=D:\New\Graduation-project\myworld
 
 if not defined PROMPT set PROMPT=$P$G
 
